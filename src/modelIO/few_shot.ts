5c98d15011bd40316f9b26f298a11b834158703a
machamp0714 <machamp0714@gmail.com>
2024-04-08 23:35:19 +0900

    Try few-show prompt

---
+++ src/modelIO/few_shot.ts
@@ -0,0 +1,40 @@
+import {
+  ChatPromptTemplate,
+  FewShotChatMessagePromptTemplate,
+} from "@langchain/core/prompts";
+
+import { ChatOpenAI } from "@langchain/openai";
+
+const model = new ChatOpenAI({});
+const examples = [
+  {
+    input: "Could the members of The Police perform lawful arrests?",
+    output: "what can the members of The Police do?",
+  },
+  {
+    input: "Jan Sindel's was born in what country?",
+    output: "what is Jan Sindel's personal history?",
+  },
+];
+const examplePrompt = ChatPromptTemplate.fromTemplate(
+  `Human: {input}\nAI: {output}`
+);
+
+const fewShotPrompt = new FewShotChatMessagePromptTemplate({
+  prefix:
+    "Rephrase the users query to be more general, using the following examples",
+  suffix: "Human: {input}",
+  examplePrompt,
+  examples,
+  inputVariables: ["input"],
+});
+
+(async () => {
+  const formattedPrompt = await fewShotPrompt.format({
+    input: "What's France's main city?",
+  });
+  console.log(formattedPrompt);
+
+  const response = await model.invoke(formattedPrompt);
+  console.log(response.content);
+})();
