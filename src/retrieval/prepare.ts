4e8b19bf27a3c29ceb2dd8dbba1fa83212f2be42
machamp0714 <machamp0714@gmail.com>
2024-04-14 13:58:41 +0900

    Try RAG

--- src/retrieval/prepare.ts
+++ src/retrieval/prepare.ts
@@ -2,6 +2,9 @@ import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { OpenAIEmbeddings } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { ChatOpenAI } from "@langchain/openai";
+import { HumanMessage } from "@langchain/core/messages";
 
 const loader = new PDFLoader("src/retrieval/asset/sample.pdf");
 
@@ -16,10 +19,34 @@ const loader = new PDFLoader("src/retrieval/asset/sample.pdf");
 
   const store = await MemoryVectorStore.fromDocuments(
     output,
-    new OpenAIEmbeddings()
+    new OpenAIEmbeddings() // embeddings はベクトル化するためのインターフェースを持っている
   );
 
-  const result = await store.similaritySearch("飛行車の最高速度は？");
-  console.log(result.length);
-  console.log(result.map((r) => r.pageContent));
+  const query = "飛行車の最高速度は？";
+  const documents = await store.similaritySearch(query);
+
+  let document = "";
+  for (const doc of documents) {
+    document += doc.pageContent;
+  }
+
+  const prompt = new PromptTemplate({
+    template: `
+      文章を元に、以下の質問に答えてください。
+
+      文章:
+      {document}
+
+      質問: {query}
+    `,
+    inputVariables: ["document", "query"],
+  });
+
+  const chat = new ChatOpenAI();
+
+  const result = await chat.invoke([
+    new HumanMessage(await prompt.format({ document, query })),
+  ]);
+
+  console.log(result.content);
 })();
